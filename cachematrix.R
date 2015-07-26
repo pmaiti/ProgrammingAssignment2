b5727a569558e4f196526a29053006986d22a5f3
pmaiti <pmaiti1970@gmail.com>
2015-07-26 09:54:40 -0700

    Getting an inverted matrix from cache
    Getting an inverted matrix from cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The following function takes a matrix as a parameter and creates a special 
+## matrix and caches in an envrionement variable
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
 
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+
+        get <- function() {
+			x
+	  }
+
+        setInverse <- function(Inverse) {
+			m <<- Inverse
+	  }
+
+        getInverse <- function() {
+			m
+	  }
+
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## The following function takes the special cached matrix as a parameter and returns
+## an invertible matrix. It returns the invertible matrix from the cache if it was 
+## already inverted and it was in the cache. Otherwise it creates an inverted matrix
+## from the scratch
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## Return a matrix that is the inverse of 'x'
+
+        m <- x$getInverse()
+        if(!is.null(m)) {
+                message("getting cached Inversed matrix")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInverse(m)
+        m
 }
